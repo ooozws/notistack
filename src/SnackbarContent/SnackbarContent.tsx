a9f97b5cbf5e937b4970ab983bcecc883099d6b8
ooozws <weisaizhang@gmail.com>
2021-07-09 04:42:54 +0800

    update material ui version

--- src/SnackbarContent/SnackbarContent.tsx
+++ src/SnackbarContent/SnackbarContent.tsx
@@ -1,24 +1,28 @@
-import React, { forwardRef } from 'react';
-import clsx from 'clsx';
-import { createStyles, Theme, withStyles, WithStyles } from '@material-ui/core/styles';
-import { SnackbarContentProps } from '../index';
+import React, { forwardRef } from "react";
+import clsx from "clsx";
+import { createStyles, Theme } from "@material-ui/core/styles";
+import { withStyles, WithStyles } from "@material-ui/styles";
+import { SnackbarContentProps } from "../index";
 
-const styles = (theme: Theme) => createStyles({
+const styles = (theme: Theme) =>
+  createStyles({
     root: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        flexGrow: 1,
-        [theme.breakpoints.up('sm')]: {
-            flexGrow: 'initial',
-            minWidth: 288,
-        },
-    }
-});
+      display: "flex",
+      flexWrap: "wrap",
+      flexGrow: 1,
+      [theme.breakpoints.up("sm")]: {
+        flexGrow: "initial",
+        minWidth: 288,
+      },
+    },
+  });
 
-interface Props extends WithStyles<typeof styles>, SnackbarContentProps { }
+interface Props extends WithStyles<typeof styles>, SnackbarContentProps {}
 
-const SnackbarContent = forwardRef<HTMLDivElement, Props>(({ classes, className, ...props }, ref) => (
-    <div ref={ref} className={clsx(classes.root, className)}  {...props} />
-))
+const SnackbarContent = forwardRef<HTMLDivElement, Props>(
+  ({ classes, className, ...props }, ref) => (
+    <div ref={ref} className={clsx(classes.root, className)} {...props} />
+  )
+);
 
 export default withStyles(styles)(SnackbarContent);
